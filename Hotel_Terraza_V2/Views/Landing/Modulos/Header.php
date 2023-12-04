b3010f708d16f79bc4dd2f7ed89d42f82cdc4eb5
Frank Rey <sarotobisensi@gmail.com>
2023-12-04 18:42:29 -0500

    se actualizo el baner y el incono

--- Hotel_Terraza_V2/Views/Landing/Modulos/Header.php
+++ Hotel_Terraza_V2/Views/Landing/Modulos/Header.php
@@ -5,12 +5,12 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Siimple - Bootstrap Landing Page Template</title>
+  <title>Hotel Terraza del Palmar</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="Assets/Plantilla/Landing/assets/img/favicon.png" rel="icon">
+  <link href="Assets/Plantilla/Landing/assets/img/logo_terraza.ico" rel="icon">
   <link href="Assets/Plantilla/Landing/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
