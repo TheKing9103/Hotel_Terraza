505629a1011f8681b32156df5e66600970a63f4b
Frank Rey <sarotobisensi@gmail.com>
2023-12-05 14:35:24 -0500

    hice un formulario de validacioon de reserva

--- Hotel_Terraza_V2/Views/Landing/Paginas/inicio.php
+++ Hotel_Terraza_V2/Views/Landing/Paginas/inicio.php
@@ -54,25 +54,76 @@
 
         <div class="row">
           <div class="col-lg-6">
-            <img src="Assets/Plantilla/Landing/assets/img/about-img.jpg" class="img-fluid" alt="">
+            
+
+      <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+
+          <?php
+          $directorio = 'Assets/Imgs/Slider/'; // Reemplaza 'tu_carpeta_de_imagenes' con la ruta de tu carpeta
+          $archivos = scandir($directorio);
+          $primera_imagen = true;
+
+          foreach ($archivos as $archivo) {
+            // Filtra solo archivos .jpg
+            if (pathinfo($archivo, PATHINFO_EXTENSION) == 'jpg') {
+              $ruta_imagen = $directorio . '/' . $archivo;
+          ?>
+
+              <div class="carousel-item img-custom-size <?php echo ($primera_imagen) ? 'active' : ''; ?>" data-bs-interval="10000">
+                <img 
+                src="<?php echo $ruta_imagen; ?>" 
+                class="img-fluid vh-100"  
+                alt="<?php echo $archivo; ?>">
+              </div>
+
+          <?php
+              $primera_imagen = false;
+            }
+          }
+          ?>
+
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+
+
           </div>
-          <div class="col-lg-6 pt-4 pt-lg-0">
-            <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
-            <p class="fst-italic">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-              magna aliqua.
+
+
+   <div class="col">
+        
+
+        <div class="text-center">
+            <h3>Visión</h3>
+            <p>Aspiramos a ser reconocidos como el referente en hospedaje rural, destacando por nuestras comodidades y una
+                ubicación privilegiada a tan solo 400 metros del casco urbano. Nos distinguimos por ofrecer precios justos,
+                promoviendo la protección del medio ambiente y garantizando la plena satisfacción de nuestros clientes.
+                Nuestra eficiencia y compromiso con la mejora continua nos posicionan como líderes en la industria, marcando
+                el estándar para una experiencia de hospedaje rural excepcional.
             </p>
-            <ul>
-              <li><i class="bx bx-check-double"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-              <li><i class="bx bx-check-double"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-              <li><i class="bx bx-check-double"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
-            </ul>
-            <p>
-              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum
+        </div>
+        <br>
+        <br>
+        <br>
+        <div class="text-center">
+            <h3>Misión</h3>
+            <p>En CASA CAMPESTRE TERRAZAS DEL PALMAR, nos dedicamos a brindar experiencias únicas de hospedaje rural.
+                Nuestro compromiso es que cada turista se sienta no solo como un visitante, sino como parte integral de un
+                entorno cómodo, seguro y tranquilo. Buscamos proporcionar oportunidades para que nuestros huéspedes se
+                integren plenamente con la naturaleza y el entorno, creando recuerdos inolvidables.
             </p>
-          </div>
+        </div>
+    </div>
+
+
         </div>
 
       </div>
@@ -83,115 +134,38 @@
       <div class="container">
 
         <div class="row">
-          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
-            <div class="card">
-              <img src="Assets/Plantilla/Landing/assets/img/why-us-1.jpg" class="card-img-top" alt="...">
-              <div class="card-icon">
-                <i class="bx bx-book-reader"></i>
-              </div>
-              <div class="card-body">
-                <h5 class="card-title"><a href="">Our Mission</a></h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-              </div>
-            </div>
+      
+<div class="container mt-5">
+  <div class="container-fluid"><br><br>
+  <div class="row d-flex justify-content-center">
+    <div class="card">
+      <h1 class="text-center">Consulta La Disponibilidad De Tu Reserva</h1>
+    </div>
+    <div class="col col-sm-2"><br><br>
+      <div class="card rounded-5">
+        <br>
+        <form method="get">
+          <label>Fecha Inicio:</label>
+          <input type="date" class="form-control" name="fecha_ini">
+          <br>
+          <label>Fecha Fin:</label>
+          <input type="date" class="form-control" name="fecha_fin">
+          <br>
+          <div class="container d-flex justify-content-center">
+            <button type="submit" class="btn btn-outline-dark">Consultar</button>
           </div>
-          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
-            <div class="card">
-              <img src="Assets/Plantilla/Landing/assets/img/why-us-2.jpg" class="card-img-top" alt="...">
-              <div class="card-icon">
-                <i class="bx bx-calendar-edit"></i>
-              </div>
-              <div class="card-body">
-                <h5 class="card-title"><a href="">Our Plan</a></h5>
-                <p class="card-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
-            <div class="card">
-              <img src="Assets/Plantilla/Landing/assets/img/why-us-3.jpg" class="card-img-top" alt="...">
-              <div class="card-icon">
-                <i class="bx bx-landscape"></i>
-              </div>
-              <div class="card-body">
-                <h5 class="card-title"><a href="">Our Vision</a></h5>
-                <p class="card-text">Nemo enim ipsam voluptatem quia voluptas sit aut odit aut fugit, sed quia magni dolores eos qui ratione voluptatem sequi nesciunt Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet. </p>
-              </div>
-            </div>
-          </div>
-        </div>
+        </form>
+        <br>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
 
       </div>
     </section><!-- End Why Us Section -->
 
-    <!-- ======= Frequenty Asked Questions Section ======= -->
-    <section class="faq">
-      <div class="container">
 
-        <div class="section-title">
-          <h2>Frequenty Asked Questions</h2>
-        </div>
-
-        <ul class="faq-list">
-
-          <li>
-            <a data-bs-toggle="collapse" class="collapsed" data-bs-target="#faq1">Non consectetur a erat nam at lectus urna duis? <i class="bx bx-down-arrow-alt icon-show"></i><i class="bx bx-x icon-close"></i></a>
-            <div id="faq1" class="collapse" data-bs-parent=".faq-list">
-              <p>
-                Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.
-              </p>
-            </div>
-          </li>
-
-          <li>
-            <a data-bs-toggle="collapse" data-bs-target="#faq2" class="collapsed">Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque? <i class="bx bx-down-arrow-alt icon-show"></i><i class="bx bx-x icon-close"></i></a>
-            <div id="faq2" class="collapse" data-bs-parent=".faq-list">
-              <p>
-                Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
-              </p>
-            </div>
-          </li>
-
-          <li>
-            <a data-bs-toggle="collapse" data-bs-target="#faq3" class="collapsed">Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi? <i class="bx bx-down-arrow-alt icon-show"></i><i class="bx bx-x icon-close"></i></a>
-            <div id="faq3" class="collapse" data-bs-parent=".faq-list">
-              <p>
-                Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus. Urna molestie at elementum eu facilisis sed odio morbi quis
-              </p>
-            </div>
-          </li>
-
-          <li>
-            <a data-bs-toggle="collapse" data-bs-target="#faq4" class="collapsed">Ac odio tempor orci dapibus. Aliquam eleifend mi in nulla? <i class="bx bx-down-arrow-alt icon-show"></i><i class="bx bx-x icon-close"></i></a>
-            <div id="faq4" class="collapse" data-bs-parent=".faq-list">
-              <p>
-                Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
-              </p>
-            </div>
-          </li>
-
-          <li>
-            <a data-bs-toggle="collapse" data-bs-target="#faq5" class="collapsed">Tempus quam pellentesque nec nam aliquam sem et tortor consequat? <i class="bx bx-down-arrow-alt icon-show"></i><i class="bx bx-x icon-close"></i></a>
-            <div id="faq5" class="collapse" data-bs-parent=".faq-list">
-              <p>
-                Molestie a iaculis at erat pellentesque adipiscing commodo. Dignissim suspendisse in est ante in. Nunc vel risus commodo viverra maecenas accumsan. Sit amet nisl suscipit adipiscing bibendum est. Purus gravida quis blandit turpis cursus in
-              </p>
-            </div>
-          </li>
-
-          <li>
-            <a data-bs-toggle="collapse" data-bs-target="#faq6" class="collapsed">Tortor vitae purus faucibus ornare. Varius vel pharetra vel turpis nunc eget lorem dolor? <i class="bx bx-down-arrow-alt icon-show"></i><i class="bx bx-x icon-close"></i></a>
-            <div id="faq6" class="collapse" data-bs-parent=".faq-list">
-              <p>
-                Laoreet sit amet cursus sit amet dictum sit amet justo. Mauris vitae ultricies leo integer malesuada nunc vel. Tincidunt eget nullam non nisi est sit amet. Turpis nunc eget lorem dolor sed. Ut venenatis tellus in metus vulputate eu scelerisque. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus faucibus. Nibh tellus molestie nunc non blandit massa enim nec.
-              </p>
-            </div>
-          </li>
-
-        </ul>
-
-      </div>
-    </section><!-- End Frequenty Asked Questions Section -->
 
     <!-- ======= Contact Us Section ======= -->
     <section id="contact" class="contact section-bg">
